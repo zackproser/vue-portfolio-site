ea9442bc7badcaed0a07d280776eaab4d652916f
Zack Proser <zproser@cloudflare.com>
2019-02-18 14:16:42 -0800

    Fix _redirects file

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -95,6 +95,11 @@ var webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../root'),
+        to: config.build.assetsRoot,
+        ignore: ['.*']
       }
     ])
   ]
